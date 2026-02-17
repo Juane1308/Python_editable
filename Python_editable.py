ce20eaee79e3e19980c4c3147a0de57f275e29fd
Camilo Mutis Arango <camilostebanmutisarango@gmail.com>
2026-02-16 23:58:52 -0500

    Agregue un Bucle

--- Python_editable.py
+++ Python_editable.py
@@ -1,3 +1,6 @@
 print('Holi Bro, soy un programa editable, puedes modificarme a tu gusto')
 print('Puedes cambiar el mensaje que te muestro, o incluso agregar nuevas funcionalidades')
 print('Hola, yo no se de que hablas, yo soy un programa fijo, no puedo ser editado')
+
+for i in range(5):
+    print(i)
