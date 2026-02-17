33e2226b6de53392fc51274e5862e8d5d94827a1
Sir_Juanes <juan.lozaguti10@gmail.com>
2026-02-16 23:54:06 -0500

    Agregue prints

--- Python_editable.py
+++ Python_editable.py
@@ -1,2 +1,3 @@
 print('Holi Bro, soy un programa editable, puedes modificarme a tu gusto')
 print('Puedes cambiar el mensaje que te muestro, o incluso agregar nuevas funcionalidades')
+print('Hola, yo no se de que hablas, yo soy un programa fijo, no puedo ser editado')
